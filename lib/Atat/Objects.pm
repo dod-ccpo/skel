e91c7e2624e2b1a8f85bca5cab9fe7d29627b69b
briandds <brian+dds@promptworks.com>
2018-04-30 13:50:56 -0400

    Domain model, objects

--- lib/Atat/Objects.pm
+++ lib/Atat/Objects.pm
@@ -1,40 +1,94 @@
+# Domain model.
+#
+# This file describes our domain using an object model.
+#
+# The following terminology may be helpful:
+#     * 'package' defines a class.
+#     * A class 'has' an attribute, which may be another object (delegation).
+#     * Plural attributes imply a 1-many (or many-many) relationship.
+#     * A class 'is' another class (vertical composition).
+#     * A class 'does' a role (horizontal composition).
+
+# User: An ATAT user.
 package User {
   use Mojo::Base -base;
-  has 'username';
-  has 'name';
+  has 'username';     # Username in ATAT
+  has 'piv';          # Data from PIV card.
+  has 'roles';        # Roles within ATAT.
 }
 
+# UserRole: Defines a set of permissions surrounding access to a CSP.
+package UserRole {
+  use Mojo::Base -base;
+  has 'csp_account';  # CSP account (optional)
+  has 'permissions';  # List of permissions (strings) for this role.
+}
+
+# Account: An account.
+package Account {
+  use Mojo::Base -base;
+  has 'csp_role';    # The role in the CSP.
+  has 'token';       # Auth token.
+}
+
+# Grant: A permission that a person has for a bucket.
+package Grant {
+  use Mojo::Base -base;
+  has 'user';           # An ATAT user.
+  has 'role';           # A UserRole (above).
+}
+
+# Bucket: An expense bucket.
 package Bucket {
   use Mojo::Base -base, -signatures;
-  has 'type';
-  has 'name';
-  has 'identifier';
-  has 'amount';
-  has 'used';
-  has 'administrators';
-
-  has 'parents' => sub { [] };    # [ { bucket => $x, amount => $y} , ... ]
+  use List::Util qw/sum/;
+  has 'id';               # Unique identifier.
+  has 'type' => 'bucket'; # String describing the type of bucket.
+  has 'name';             # Name of the bucket.
+  has 'limit';            # Limit on usage for this bucket.
+  has 'used';             # Amount of money used.
+  has 'grants';           # List of Grants for this bucket.
+  has 'distribute_into';  # A list of other buckets into which this one can be distributed.
+  has '_aggregated_into'; # A single parent bucket into which this one is aggregated.
+  has '_aggregated_from'  # The inverse of the above.
+     => sub { [] };
+  has 'csp_tags';         # Tags in the CSP associated with this bucket.
 
   sub remaining ($self) {
-    $self->amount - $self->used;
+    $self->limit - ( $self->used || 0 );
   }
 
-  sub add_parent ($self, $bucket, $amount) {
-    my $allocation = Allocation->new(bucket => $bucket, amount => $amount);
-    push @{$self->parents}, $allocation;
+  sub aggregate_to ($self, $bucket) {
+      $self->_aggregated_into([ $bucket ]);
+      push @{ $bucket->_aggregated_from }, $self;
   }
+
+  sub distribute_to ($self, $bucket, $amount) {
+    push @{$self->distributed_into},
+        Distribution->new(bucket => $bucket, amount => $amount);
+  }
+
+  sub calculate_used ($self, %args ) {
+    return $self->used if defined( $self->used );
+    my @children = @{ $self->_aggregated_from || [] };
+    my $sum = sum map $_->calculate_used( %args ), @children;
+    $self->used( $sum ) if $args{store};
+    return $sum;
+   }
 }
 
-package Allocation {
+# A bucket can have an amount distributed to another bucket.
+package Distribution {
   use Mojo::Base -base;
   has 'bucket';
   has 'amount';
 }
 
-package Administrator {
-  use Mojo::Base -base;
-  has 'user';
-  has 'role';
+# A TaskOrder is a bucket.
+package TaskOrder {
+  use Mojo::Base 'Bucket';
+  has 'type' => 'task_order';
+  has 'clins' => sub { [] };    # List of amount and clin
 }
 
 1;
--- t/02-domain.t
+++ t/02-domain.t
@@ -1,25 +1,51 @@
 use Test::More;
 use Atat::Objects;
 
-my $u = User->new(username => 'alice', name => 'Alice');
-is $u->name,     'Alice', 'Set name';
-is $u->username, 'alice', 'Set username';
+# Users and roles
+my $alice = User->new(username => 'alice');
+is $alice->username, 'alice', 'Set username';
+$alice->piv({identifier => 101, serial_number => 23});
+is $alice->piv->{identifier}, 101, 'set some PIV data';
+my $bob = User->new(username => 'bob');
+is $bob->username, 'bob', 'Made Bob';
 
-my $b = Bucket->new;
-$b->amount(200_000);
-$b->used(10_000);
-$b->administrators([$u]);
+# ATAT permissions for Alice and Bob
+$alice->roles(
+  [
+    UserRole->new(
+      csp_account => 100,
+      permissions => [qw/create_bucket manage_roles create_user/]
+    ),
+    UserRole->new(csp_account => 101, permissions => [qw/access_cloud/]),
+  ]
+);
+$bob->roles(
+  [UserRole->new(csp_account => 100, permissions => [qw/access_cloud/])]);
+is $alice->roles->[0]->csp_account, 100, 'Set csp account for Alice';
+is $bob->roles->[0]->csp_account,   100, 'Set csp account for Bob';
 
-is $b->amount,    200_000, 'set amount';
-is $b->used,      10_000,  'used';
-is $b->remaining, 190_000, 'calculated remaining';
-is $b->administrators->[0]->name, 'Alice', 'Set administrator';
+# Buckets, distributions, and aggregations.
+my $task_order = TaskOrder->new(limit => 200_000);
 
-my $child = Bucket->new(amount => 50_000);
-$child->add_parent($b, 50_000);
+# Aggregate from team to mission to task order.
+my $mission = Bucket->new( type => 'mission', name => "Capture the flag");
+my $blue = Bucket->new( name => 'Blue team', type => 'team', id => 1);
+$blue->aggregate_to($mission);
+my $red = Bucket->new( name => 'Red team', type => 'team', id => 2);
+$red->aggregate_to($mission);
+$mission->aggregate_to($task_order);
 
-isa_ok $child->parents->[0], 'Allocation', 'made an allocation';
-is $child->parents->[0]->amount, 50_000, 'set child bucket';
+# Set amount used by red and blue team, then aggregate up.
+$red->used(10_000);
+$blue->used(15_000);
+
+# Run calculation, store mission and task order aggregates.
+$task_order->calculate_used( store => 1);
+
+# Check results.
+is $mission->used, 25_000, 'Aggregated team into mission';
+is $task_order->used, 25_000,'Aggregated mission into task order';
+is $task_order->remaining, 175_000, 'Calculated remaining';
 
 done_testing;
 
